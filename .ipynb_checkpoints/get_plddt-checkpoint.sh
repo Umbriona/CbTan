79c9187841d0c2995f7d36bcd3cf46ce96d37c82
Umbriona <33811221+Umbriona@users.noreply.github.com>
2023-03-23 15:29:00 +0100

    Rename everything to remove ileagal characters

---
+++ .ipynb_checkpoints/get_plddt-checkpoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+
+
+
+
+
+for ranked in 0 1 2 3 4
+do
+    file_output="results/plddt/plddt_ranked_${ranked}.csv"
+    echo ID,plddt_avg,plddt,aa_pos > ${file_output}
+    for FILE in data/structures/ranked_${ranked}/*
+    do      
+         PLDDT_VEC=$(cat ${FILE} | cut -c 62- | cut -f 1 -d " " | tr '\n' ';')
+         PLDDT_MEAN=$(cat ${FILE} | cut -c 62- | cut -f 1 -d " " | awk ' {total+=$1} END {print total/NR}')
+         AA_POS=$(cat ${FILE}| cut -c 24- | rev | cut -c 55- | rev | sed  -r s'/^[ ]+//g'| tr '\n' ';')  
+         ID=$(echo ${FILE} | rev | cut -f 1 -d "/"| rev)
+         echo ${ID}
+         echo ${ID},${PLDDT_MEAN},${PLDDT_VEC},${AA_POS} >> ${file_output}
+    done
+done
